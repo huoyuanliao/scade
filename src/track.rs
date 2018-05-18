6772677a1e7642e8c5a8c01fef3d8f12b15c7a83
liaozy <798389647@qq.com>
2018-05-18 22:11:06 +0800

    tracker init

--- src/track.rs
+++ src/track.rs
@@ -5,6 +5,7 @@ use std::collections::HashSet;
 type IpSet = HashSet<IpAddr>;
 type FailCounts = [u32; 4];
 type Port = u16;
+type Protocol=u8;
 type PortIpSet = HashMap<Port, Box<IpSet>>;
 
 struct Tracker {
@@ -13,8 +14,70 @@ struct Tracker {
 
     ip_fail_counts: HashMap<IpAddr, FailCounts>,
     port_other_list:PortIpSet,
-    port_icmp_list: Box<IpSet>,
+    port_icmp_list: IpSet,
 
     tcp_sm_list: PortIpSet,
     udp_sm_list: PortIpSet,
+}
+
+impl Tracker {
+    pub fn new( scanner: IpAddr) -> Tracker {
+        Tracker {
+            tracker_ip: scanner,
+            total_fail_counts: [0,0,0,0],
+            ip_fail_counts: HashMap::new(),
+            port_other_list: HashMap::new(),
+            port_icmp_list: IpSet::new(),
+
+            tcp_sm_list: PortIpSet::new(),
+            udp_sm_list: PortIpSet::new(),
+        }
+    }
+
+    fn get_port_type(self, port: Port, proto: Protocol) {
+        if proto == 0x6 {
+            //tcp
+        }
+        else if proto == 0x11 {
+            //udp
+        }
+        else if proto == 0x1 {
+            //icmp
+        }
+        else {
+            //other
+        }
+    }
+
+    fn __tcp_sm_list_add(self, ip: IpAddr, port: Port) {
+
+    }
+
+    fn __udp_sm_list_add(self, ip: IpAddr, port: Port) {
+
+    }
+
+    fn __other_list_append(self, ip: IpAddr, port: Port) {
+        
+    }
+
+    fn __icmp_list_append(self, ip: IpAddr) {
+
+    }
+
+    fn __total_failcounts_add(self, port_type: u8) {
+
+    }
+
+    fn __ip_pool_append(self, ip: IpAddr, port_type: u8) {
+
+    }
+
+    pub fn track_scanned(self, ip: IpAddr, port: Port, proto: Protocol) {
+
+    }
+
+    pub fn get_ip_failcounts_amount(self) {
+        //return ip_failcounts amount
+    }
 }
